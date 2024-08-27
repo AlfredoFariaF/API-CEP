074bb0d2115ab510ebee936a5b42e2dec15835cd
AlfredoFariaF <alfredo.ferreira@live.com>
2024-08-27 08:48:48 -0300

    mudança trabalho
    mudança

--- login.php
+++ login.php
@@ -7,8 +7,6 @@ $senha=$_POST["senha"];
 $sql= "select nome from usuarios where cpf = '$cpf' and senha = '$senha' ";
 $resultado = $conn->query($sql);
 $row = $resultado->fetch_assoc();
-//alfredo seu fdm fandangos de morango
-//vai se lascar
 if(isset($row) && $row["nome"] != ''){
     session_start();
     $_SESSION["cpf"] = $cpf;
