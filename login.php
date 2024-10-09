affbb63d67cd66deaa5425398365d91bf4eacb99
AlfredoFariaF <alfredo.ferreira@live.com>
2024-10-09 08:55:44 -0300

    SQL_INJECT

--- login.php
+++ login.php
@@ -4,18 +4,31 @@ include("conexao.php");
 $cpf=$_POST["cpf"];
 $senha=$_POST["senha"];
 
-$sql= "select nome from usuarios where cpf = '$cpf' and senha = '$senha' ";
-$resultado = $conn->query($sql);
-$row = $resultado->fetch_assoc();
-if(isset($row) && $row["nome"] != ''){
-    session_start();
-    $_SESSION["cpf"] = $cpf;
-    $_SESSION["senha"] = $senha;
-    $_SESSION["nome"] = $row["nome"];
+$sql= "select nome from usuarios where cpf = ? and senha = ? ";
+$stmt = $conn->prepare($sql);
 
-    header("Location: primeiro.php");
-}else{
-    die("SENHA INCORRETA");
+if($stmt){
+    $stmt->bind_param("ss", $cpf, $senha);
+    $stmt->execute();
+    $result = $stmt->get_result();
+
+    if($result->num_rows > 0){
+        $row = $result->fetch_assoc();
+
+        if($row["nome"] != ''){
+
+            session_start();
+            $_SESSION["cpf"] = $cpf;
+            $_SESSION["senha"] = $senha;
+            $_SESSION["nome"] = $row["nome"];
+        
+            header("Location: primeiro.php");
+        }else{
+            die("SENHA INCORRETA");
+        }
+    }else{
+        die("NENHUM USUÁRIO ENCONTRADO");
+    }
 }
 
 ?>
